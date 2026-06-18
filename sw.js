75ff33b310e23cc388b6bec0f37ca2faba31882d
yaaqob775-star <yaaqob775@gmail.com>
2026-06-18 05:47:00 +0300

    Change cache name and modify fetch event
    Updated cache name and simplified fetch handling.

--- sw.js
+++ sw.js
@@ -1,18 +1,6 @@
-const CACHE_NAME = "ai-tools-v2";
-
-const FILES = [
-  "./",
-  "./index.html",
-  "./chat.html",
-  "./pdf.html",
-  "./image.html",
-  "./manifest.json"
-];
+const CACHE_NAME = "ai-tools-v3";
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
-  );
   self.skipWaiting();
 });
 
@@ -21,7 +9,5 @@ self.addEventListener("activate", event => {
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
-  );
+  event.respondWith(fetch(event.request));
 });
